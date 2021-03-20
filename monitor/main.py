e3bfc3116d23e4ac2fb7a0a71b4ef46fc75bb542
zkdev <dev@zeekay.dev>
2021-03-20 16:51:40 +0100

    Insert monitoring to db

--- monitor/main.py
+++ monitor/main.py
@@ -20,14 +20,51 @@ util.configure_default_logging(stdout_level=logging.INFO)
 
 
 def _monitor_status_endpoint(
-    endpoint_url: str,
+    component_config: model.ComponentConfig,
 ):
+    endpoint_url = util.urljoin(
+        component_config.baseUrl,
+        component_config.statusEndpoint,
+    )
     logger.info(f'monitoring {endpoint_url}')
     resp = requests.get(
         url=endpoint_url,
     )
     resp_time = resp.elapsed.total_seconds()
     logger.debug(f'{resp.status_code} in {resp_time}')
+
+    conn = database.get_connection()
+    frame_id = database.select_frame_id(
+        conn=conn,
+        component=component_config.id,
+    )
+    cf = _parse_response_to_component_frame(
+        resp=resp,
+        component_config=component_config,
+        frame_id=frame_id,
+    )
+    database.insert_component_frame(
+        conn=conn,
+        component_frame=cf,
+    )
+    database.kill_connection(
+        conn=conn,
+    )
+
+
+def _parse_response_to_component_frame(
+    resp,
+    component_config: model.ComponentConfig,
+    frame_id: int,
+) -> model.ComponentFrame:
+    cf = model.ComponentFrame(
+        component=component_config.id,
+        frame=frame_id,
+        timestamp='now()',
+        reachable=True if resp.status_code == 200 else False,
+        responseTime=resp.elapsed.total_seconds(),
+    )
+    return cf
 
 
 def _schedule_event_loops(
@@ -40,26 +77,17 @@ def _schedule_event_loops(
         if c.frequency.__contains__('s'):
             schedule.every(int(c.frequency.replace('s', ''))).seconds.do(
                 _monitor_status_endpoint,
-                endpoint_url=util.urljoin(
-                    c.baseUrl,
-                    c.statusEndpoint,
-                )
+                component_config=c,
             )
         elif c.frequency.__contains__('m'):
             schedule.every(int(c.frequency.replace('m', ''))).minutes.do(
                 _monitor_status_endpoint,
-                endpoint_url=util.urljoin(
-                    c.baseUrl,
-                    c.statusEndpoint,
-                )
+                component_config=c,
             )
         elif c.frequency.__contains__('h'):
             schedule.every(int(c.frequency.replace('h', ''))).hours.do(
                 _monitor_status_endpoint,
-                endpoint_url=util.urljoin(
-                    c.baseUrl,
-                    c.statusEndpoint,
-                )
+                component_config=c,
             )
 
 
